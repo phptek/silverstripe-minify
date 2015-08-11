8471149a42745627f8939c02bd136b43edb8cd99
Russell Michell <russell.michell@deviate.net.nz>
2015-08-12 10:51:06 +1200

    MINOR: - Made $rewrite_uris static, private for SS' config system - Added missing comments - Fixed SS coding guideline violations - Only single call to increase_time_limit_to() required

--- code/MinifyRequirements.php
+++ code/MinifyRequirements.php
@@ -1,31 +1,44 @@
 <?php
-
+/**
+ * 
+ * An alternative {@link Requirements} backend for SilverStripe for the provision
+ * of a simple file-minification API for both JavaScript and CSS files.
+ * 
+ * @author Nathan Cox
+ * @package silverstripe-minify
+ */
 class Minify_Requirements_Backend extends Requirements_Backend {
 
-	static $rewrite_uris = true;
+	/**
+	 *
+	 * @var boolean 
+	 */
+	private static $rewrite_uris = true;
 
+	/**
+	 * 
+	 * @param string $filename
+	 * @param string $content
+	 * @return string
+	 */
 	protected function minifyFile($filename, $content) {
-
-		// if we have a javascript file and jsmin is enabled, minify the content
+		// If we have a javascript file and jsmin is enabled, minify the content
 		$isJS = stripos($filename, '.js');
+		increase_time_limit_to();
+		
 		if($isJS && $this->combine_js_with_jsmin) {
-			//require_once('thirdparty/jsmin/jsmin.php');
-
-			increase_time_limit_to();
 			$content = JSMin::minify($content);
-		} else if (stripos($filename, '.css')) {
-
-			increase_time_limit_to();
+		} elseif(stripos($filename, '.css')) {
 			$minifyCSSConfig = array();
 
-			if (self::$rewrite_uris) {
+			if(self::$rewrite_uris) {
 				$minifyCSSConfig['currentDir'] = Director::baseFolder() . '/' . dirname($filename);
 			}
 
 			$content = Minify_CSS::minify($content, $minifyCSSConfig);
 		}
 
-		$content .= ($isJS ? ';' : '') . "\n";
+		$content .= ($isJS ? ';' : '') . PHP_EOL;
 		return $content;
 	}
 
